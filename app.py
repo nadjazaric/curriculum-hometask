008925952d82d105b1e84572f2396ca6106b6d2f
nadjazaric <nadjazaric@gmail.com>
2023-02-22 22:04:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-import datetime
-from flask import Flask, render_template
+import datetime, json, os
+from flask import Flask, render_template, request,  redirect, url_for, flash
 
 app = Flask(__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/")
+comments = []
+formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+filename = os.path.join(app.static_folder, 'data', 'spam.json')
+with open(filename) as test_file:
+    spamData = json.load(test_file)
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        comment_content = request.form.get("content")
+        user_name = request.form.get("userName")
+        for i in spamData:
+            if i in comment_content:
+                flash('Your message looks like spam. Please rephrase it')
+                return redirect(url_for('home'))
+        comments.append((comment_content, formatted_date, user_name))
+    return render_template("home.html", comments=comments, spamData=spamData)
+
+
+@app.get("/recent")
+def recentPage():
+    return render_template("recent.html", comments=comments)
+
