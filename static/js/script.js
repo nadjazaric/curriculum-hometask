69e3b551c31ce357a29e7496c6b08156f90f6bd2
nadjazaric <nadjazaric@gmail.com>
2023-02-22 22:03:31 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,16 @@
 
+var likesIcon = document.querySelector(".noLikes");
+var noLikes = document.querySelector(".noLikes b");
+var commentIcon = document.querySelector(".noComments")
+
+count = 0;
+likesIcon.addEventListener('click', function(){
+  count = parseInt(noLikes.textContent) + 1;
+  noLikes.textContent = count;
+});
+
+commentIcon.addEventListener('click', function(){
+   document.querySelector(".addComment").style.display = "block";
+  });
+
+  
